022a105ab16dc4aa8149033e8d8d1ba6fbd2bfde
Jeaner Silva <jeaner.silva@abingov.onmicrosoft.com>
2024-02-13 07:41:30 -0300

    Parêntesis

--- respostas/substitui.sh
+++ respostas/substitui.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-
 # Função para substituir uma expressão por outra dentro de cada arquivo
 replace_in_files() {
     folder="$1"
     antes="$2"
     depois="$3"
-    echo "Substitui $antes por $depois em $folder"
-    find "$folder" -type f -name "*.txt" -exec sed -i "s/$antes:/$depois/g" {} \;
+    expressao="s/$antes/$depois/g"
+    # echo "Substitui $antes por $depois em $folder"
+    find "$folder" -type f -name "*.txt" -exec sed -i "$expressao" {} \;
 }
 
-# Navega para o diretório raiz onde estão os subdiretórios
-#cd /caminho/do/diretorio/raiz
 
 # Chama a função para substituir a expressão em cada subdiretório
 for subfolder in */; do
-    replace_in_files "$subfolder" "1)" "1."
+    for i in {1..10}; do
+        echo "$subfolder" "$i)" "$i."
+        replace_in_files "$subfolder" "$i)" "$i."
+    done
+    # replace_in_files "$subfolder" "1)" "1."
+    
 done
